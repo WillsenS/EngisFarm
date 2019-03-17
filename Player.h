0632ed7f44364a886590416948f1aa87964fd0e5
Willsen Sentosa <38328717+WillsenS@users.noreply.github.com>
2019-03-18 04:18:49 +0700

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,43 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <string>
+
+class Player {//: public livingThings {
+    private:
+        int posX;
+        int posY;
+        int water;
+        int inventory[100]; //(?) 
+    public:
+        //Constructor
+        Player(int x, int y);
+
+        //Destructor
+        ~Player();
+
+        //Bergerak ke posX, posY
+        //Hanya dapat bergerak 1 petak per pemanggilan (?) ato enggak
+        void Move (int posX, int posY);
+
+        //Berbicara kepada hewan,
+        //Suara dihasilkan bergantung pada hewan
+        //Apa perlu animal ada method talk?
+        void Talk();
+
+        //Berinteraksi dengan objek di samping player
+        //Dapat berupa FarmAnimal atau Facility
+        //Untuk FarmAnimal, hanya dapat digunakan kepada EggProducingAnimal dan MilkProducingAnimal
+        void Interact();
+
+        //Menyembelih hewan
+        //Hanya dapat digunakan kepada MeatProducingAnimal
+        void Kill();
+
+        //Menyiram land
+        //Menumbuhkan rumput ditempat player berdiri
+        void Grow();
+
+};
+#endif
