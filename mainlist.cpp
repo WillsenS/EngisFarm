2b8166ad0934243c2a4bd245d18ff28c5e2e946d
Josep Andre Ginting <josepandregintings@gmail.com>
2019-03-18 18:23:28 +0700

    Update mainlist.cpp

--- mainlist.cpp
+++ mainlist.cpp
@@ -6,24 +6,12 @@ using namespace std;
 int main(){
     LinkedList<int> x;
     x.print();
-    if(x.isEmpty()){
-        cout << "Kosong" << endl;
-    }
+    
     x.add(1);
+    x.add(2);
+    x.remove(1);
     cout << x.getSize() << endl;
     x.print();
-    x.add(3);
-    cout << x.getSize() << endl;
-    x.print();
-    x.add(7);
-    cout << x.getSize() << endl;
-    x.print();
-    cout << x.find(2) << endl;
-    cout << x.find(7) << endl;
-    x.print();
-    if(x.get(0)==1){
-        cout << "get" << endl;
-    }
     
     return 0;
 }
