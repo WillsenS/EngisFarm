536f3f01beb2f438218a5ef09d733181b180c6d9
gha-gozu <ghazwan.sihamudin@gmail.com>
2019-03-18 14:29:37 +0700

    add(product): Add product header files.

---
+++ BBQ.h
@@ -0,0 +1,20 @@
+#ifndef BBQ_H
+#define BBQ_H
+
+#include "SideProduct.h"
+
+/**
+ * BBQ class.
+ * 
+ * @author Ghazwan S. M. <ghazwan.sihamudin@gmail.com>
+ * @since 2019.03.17
+ */
+Class BBQ : public SideProduct {
+    public:
+        BBQ();
+        ~BBQ();
+
+    private:
+
+};
+#endif
---
+++ ChickenMeat.h
@@ -0,0 +1,20 @@
+#ifndef CHICKENMEAT_H
+#define CHICKENMEAT_H
+
+#include "FarmProduct.h"
+
+/**
+ * ChickenMeat class.
+ * 
+ * @author Ghazwan S. M. <ghazwan.sihamudin@gmail.com>
+ * @since 2019.03.17
+ */
+Class ChickenMeat : public FarmProduct {
+    public:
+        ChickenMeat();
+        ~ChickenMeat();
+
+    private:
+
+};
+#endif
