51ae5742577a668c44da2fcefbfc927fb36216b3
Willsen Sentosa <38328717+WillsenS@users.noreply.github.com>
2019-04-03 16:45:13 +0700

    Update Player.h

--- include/Player.h
+++ include/Player.h
@@ -2,42 +2,44 @@
 #define PLAYER_H
 
 #include <iostream>
+#include <string>
+#include "LinkedList.h"
 #include "LivingThings.h"
 
-class Player: public LivingThings{
+class Player : public LivingThings {
     private:
         int posX;
         int posY;
         int water;
         int money;
-    
+        LinkedList<string> inventory;
     public:
-        /*Constructor */
+        //Constructor
         Player(int x, int y);
 
-        /*Destructor*/
+        //Destructor
         ~Player();
 
-        /*Bergerak ke posX, posY*/
-        void Move (int posX, int posY);
+        //Bergerak ke posX, posY
+        //Hanya dapat bergerak 1 petak per pemanggilan (?) ato enggak
+        void move (int posX, int posY);
 
-        /*Berbicara kepada hewan,
-          Suara dihasilkan bergantung pada hewan*/
-        void Talk();
+        // //Berbicara kepada hewan,
+        // //Suara dihasilkan bergantung pada hewan
+        // void Talk();
 
-        /*Berinteraksi dengan objek di samping player
-          Dapat berupa FarmAnimal atau Facility
-          Untuk FarmAnimal, hanya dapat digunakan kepada EggProducingAnimal dan MilkProducingAnimal*/
-        void Interact();
+        // //Berinteraksi dengan objek di samping player
+        // //Dapat berupa FarmAnimal atau Facility
+        // //Untuk FarmAnimal, hanya dapat digunakan kepada EggProducingAnimal dan MilkProducingAnimal
+        // void Interact();
 
-        /*Menyembelih hewan
-          Hanya dapat digunakan kepada MeatProducingAnimal*/
-        void Kill();
+        // //Menyembelih hewan
+        // //Hanya dapat digunakan kepada MeatProducingAnimal
+        // void Kill();
 
-        /*Menyiram land
-          Menumbuhkan rumput ditempat player berdiri
-          Water -1*/
-        void Grow();
-
+        // //Menyiram land
+        // //Menumbuhkan rumput ditempat player berdiri
+        // //Water -1
+        // void Grow();
 };
 #endif
